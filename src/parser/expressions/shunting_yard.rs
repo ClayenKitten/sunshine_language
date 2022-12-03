298cc14c41acb39c2b3c9be869ac3f4b6aa959b4
LowLevelVirtualMan <dima070704@yandex.ru>
2022-12-04 01:08:34 +0300

    Break shunting yard parsing instead of error on unmatched parenthesis

--- src/parser/expressions/shunting_yard.rs
+++ src/parser/expressions/shunting_yard.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use crate::{lexer::{punctuation::Punctuation, Token, TokenStream}, parser::{ParserError, UnexpectedTokenError}};
+use crate::{lexer::{punctuation::Punctuation, Token, TokenStream}, parser::ParserError};
 
 use super::Expression;
 
@@ -22,13 +22,10 @@ impl ReversePolishExpr {
             match lexer.peek()? {
                 Token::Punctuation(punc) if punc.0 == "(" => {
                     lexer.next()?;
-
                     is_last_token_an_operand = false;
-                    op_stack.push(Operator::LeftParenthesis)
+                    op_stack.push(Operator::LeftParenthesis);
                 }
                 Token::Punctuation(punc) if punc.0 == ")" => {
-                    lexer.next()?;
-
                     while let Some(top_op) = op_stack.last() {
                         if top_op == &Operator::LeftParenthesis {
                             break;
@@ -38,10 +35,10 @@ impl ReversePolishExpr {
                     
                     // Either `op_stack` is empty or left parenthesis is on the top at that point.
                     if op_stack.pop().is_none() {
-                        // Expected left parenthesis
-                        return Err(UnexpectedTokenError::TokenMismatch.into())
+                        break;
                     }
                     is_last_token_an_operand = true;
+                    lexer.next()?;
                 }
                 Token::Punctuation(punc) if punc.is_operator() => {
                     let arity = if is_last_token_an_operand && punc.is_binary_operator() {
@@ -90,8 +87,6 @@ impl ReversePolishExpr {
         
         Ok(ReversePolishExpr(output))
     }
-
-    
 }
 
 #[derive(Debug, PartialEq, Eq)]
