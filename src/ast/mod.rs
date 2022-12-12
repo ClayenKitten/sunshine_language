cf29493b1a3aef8cf192d8f2df73127d3fb3df69
LowLevelVirtualMan <dima070704@yandex.ru>
2022-12-12 12:57:39 +0300

    Impl `Display` for `Identifier`

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub mod expression;
 pub mod item;
 pub mod statement;
@@ -5,3 +7,9 @@ pub mod statement;
 /// Identifier is name of type, variable or function.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Identifier(pub String);
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
