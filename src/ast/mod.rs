d28df6a7828639094d872be0d3e4f53471142cce
LowLevelVirtualMan <dima070704@yandex.ru>
2022-12-25 01:14:35 +0300

    Add `FromStr` impl for `Identifier`

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,4 +1,5 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
+use thiserror::Error;
 
 pub mod expression;
 pub mod item;
@@ -12,6 +13,34 @@ impl Display for Identifier {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
+}
+
+impl FromStr for Identifier {
+    type Err = IdentifierParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {        
+        if s.is_empty() {
+            return Err(IdentifierParseError::Empty);
+        }
+        if s.starts_with(|ch: char| ch.is_ascii_digit()) {
+            return Err(IdentifierParseError::StartsWithNumber);
+        }
+        if let Some(ch) = s.chars().find(|ch| !(ch.is_ascii_alphanumeric() || *ch == '_')) {
+            return Err(IdentifierParseError::InvalidCharacter(ch));
+        }
+        
+        Ok(Identifier(s.to_string()))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Error)]
+pub enum IdentifierParseError {
+    #[error("identifier shouldn't start with a number")]
+    StartsWithNumber,
+    #[error("identifier may only contain ascii alphanumeric and underscore characters, character `{0}` met")]
+    InvalidCharacter(char),
+    #[error("identifier can't be empty")]
+    Empty,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
