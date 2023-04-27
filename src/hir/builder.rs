7811fc9c9c21e7ff48d21a7100e143c0efe1af58
LowLevelVirtualMan <dima070704@yandex.ru>
2023-04-27 18:01:39 +0300

    Extract body translation to submodule of `hir/builder`

--- src/hir/builder.rs
+++ src/hir/builder.rs
@@ -1,23 +1,21 @@
+mod body;
+
 use std::collections::HashMap;
 
 use crate::{
     ast::{
-        expression::Block as AstBlock,
-        expression::Expression as AstExpression,
         item::Function as AstFunction,
-        item::{ItemKind, Parameter, Field},
-        statement::LetStatement,
-        statement::Statement as AstStatement,
+        item::{Field, ItemKind},
     },
-    hir,
-    path::AbsolutePath,
     item_table::ItemTable,
+    path::AbsolutePath,
 };
 
+use self::body::BodyBuilder;
+
 use super::{
-    scope::Scope,
-    types::{TypeError, TypeTable, TypeId},
-    Expression, Function, FunctionId, Hir, Statement, Block, FunctionSignature,
+    types::{TypeError, TypeId, TypeTable},
+    Function, FunctionId, Hir,
 };
 
 use thiserror::Error;
@@ -28,8 +26,6 @@ pub struct HirBuilder {
     function_mapping: HashMap<AbsolutePath, FunctionId>,
     functions: Vec<Function>,
     errors: Vec<TranslationError>,
-    scope: Scope,
-    current_function: Option<AbsolutePath>,
 }
 
 impl HirBuilder {
@@ -72,124 +68,17 @@ impl HirBuilder {
             }
         }
 
-        for (path, function) in functions {
-            self.current_function = Some(path);
-            let function = self.translate_func(function);
-            match function {
+        for (mut path, function) in functions {
+            path.pop();
+            match BodyBuilder::translate(&self, path, function) {
                 Ok(function) => self.functions.push(function),
                 Err(error) => self.errors.push(error),
             }
-            self.current_function = None;
         }
     }
 
     pub fn new() -> Self {
         Self::default()
-    }
-
-    fn translate_func(&mut self, func: AstFunction) -> Result<Function, TranslationError> {
-        let AstFunction {
-            name: _,
-            params,
-            return_type,
-            body,
-        } = func;
-
-        let signature = FunctionSignature {
-            params: params
-                .into_iter()
-                .map(|Parameter { name, type_ }| Ok::<_, TypeError>((name, self.type_table.get(type_)?)))
-                .collect::<Result<_, _>>()?,
-            return_type: match return_type {
-                Some(type_) => Some(self.type_table.get(type_)?),
-                None => None,
-            },
-        };
-
-        Ok(Function {
-            signature,
-            body: self.translate_block(body)?,
-        })
-    }
-
-    fn translate_block(&mut self, block: AstBlock) -> Result<Block, TranslationError> {
-        self.scope = self.scope.child();
-        let block = {
-            let mut result = Vec::new();
-            for stmt in block.statements {
-                let stmt = self.translate_stmt(stmt)?;
-                result.push(stmt);
-            }
-            if let Some(expr) = block.expression {
-                let expr = self.translate_expr(*expr)?;
-                result.push(hir::Statement::Return(expr))
-            }
-            Ok(hir::Block(result))
-        };
-        self.scope = self.scope.parent().unwrap_or_default();
-        block
-    }
-
-    fn translate_stmt(&mut self, stmt: AstStatement) -> Result<Statement, TranslationError> {
-        match stmt {
-            AstStatement::ExprStmt(expr) => {
-                self.translate_expr(expr).map(Statement::ExprStmt)
-            }
-            AstStatement::LetStmt(LetStatement { name, type_, value }) => {
-                let Some(type_) = type_ else { return Err(TranslationError::TypeInference)};
-                let type_ = self.type_table.get(type_)?;
-                let value = value
-                    .map(|v| *v)
-                    .and_then(|expr| self.translate_expr(expr).ok())
-                    .map(Box::new);
-                Ok(hir::Statement::LetStmt { name, type_, value })
-            }
-            AstStatement::Assignment {
-                assignee,
-                operator,
-                expression,
-            } => todo!(),
-            AstStatement::Return(expr) => {
-                self.translate_expr(expr).map(Statement::Return)
-            }
-            AstStatement::Break => todo!(),
-        }
-    }
-
-    fn translate_expr(&mut self, expr: AstExpression) -> Result<Expression, TranslationError> {
-        Ok(match expr {
-            AstExpression::Block(block) => {
-                hir::Expression::Block(self.translate_block(block)?)
-            }
-            AstExpression::If {condition, body, else_body} => todo!(),
-            AstExpression::While { condition, body } => todo!(),
-            AstExpression::For { var, expr, body } => todo!(),
-            AstExpression::Unary { op, value } => todo!(),
-            AstExpression::Binary { op, left, right } => todo!(),
-            AstExpression::FnCall { path, params: ast_params } => {
-                let mut params = Vec::with_capacity(ast_params.capacity());
-                for param in ast_params {
-                    params.push(self.translate_expr(param)?);
-                }
-                
-                let path = {
-                    let mut context_path = self.current_function.clone()
-                    .expect("`current_function` should be set");
-                    context_path.pop();
-                    let Some(path) = path.to_absolute(&context_path) else {
-                        todo!();
-                    };
-                    path
-                };
-
-                let Some(id) = self.function_mapping.get(&path).copied() else {
-                    return Err(TranslationError::FunctionNotFound(path.clone()));
-                };
-                Expression::FnCall(id, params)
-            }
-            AstExpression::Var(name) => todo!(),
-            AstExpression::Literal(lit) => Expression::Literal(lit),
-        })
     }
 }
 
---
+++ src/hir/builder/body.rs
@@ -0,0 +1,132 @@
+use std::collections::HashMap;
+
+use crate::{
+    ast::{
+        expression::Block as AstBlock, expression::Expression as AstExpression,
+        item::Function as AstFunction, item::Parameter, statement::LetStatement,
+        statement::Statement as AstStatement,
+    },
+    hir::{
+        scope::Scope, Block, Expression, Function, FunctionSignature, HirBuilder,
+        Statement, TranslationError,
+    },
+    path::AbsolutePath,
+};
+
+pub(super) struct BodyBuilder<'b> {
+    parent: &'b HirBuilder,
+    module: AbsolutePath,
+    scope: Scope,
+}
+
+impl<'b> BodyBuilder<'b> {
+    pub fn translate(
+        parent: &'b HirBuilder,
+        module: AbsolutePath,
+        function: AstFunction,
+    ) -> Result<Function, TranslationError> {
+        let mut builder = Self {
+            parent,
+            module,
+            scope: Scope::new(),
+        };
+        builder.translate_function(function)
+    }
+
+    fn translate_function(&mut self, func: AstFunction) -> Result<Function, TranslationError> {
+        let AstFunction {
+            name: _,
+            params,
+            return_type,
+            body,
+        } = func;
+
+        let mut parameters = HashMap::with_capacity(params.len());
+        for Parameter { name, type_ } in params {
+            parameters.insert(name, self.parent.type_table.get(type_)?);
+        }
+
+        let signature = FunctionSignature {
+            params: parameters,
+            return_type: match return_type {
+                Some(type_) => Some(self.parent.type_table.get(type_)?),
+                None => None,
+            },
+        };
+
+        Ok(Function {
+            signature,
+            body: self.translate_block(body)?,
+        })
+    }
+
+    fn translate_block(&mut self, block: AstBlock) -> Result<Block, TranslationError> {
+        self.scope = self.scope.child();
+        let block = {
+            let mut result = Vec::new();
+            for stmt in block.statements {
+                let stmt = self.translate_stmt(stmt)?;
+                result.push(stmt);
+            }
+            if let Some(expr) = block.expression {
+                let expr = self.translate_expr(*expr)?;
+                result.push(Statement::Return(expr))
+            }
+            Ok(Block(result))
+        };
+        self.scope = self.scope.parent().expect("Scope should have parent");
+        block
+    }
+
+    fn translate_stmt(&mut self, stmt: AstStatement) -> Result<Statement, TranslationError> {
+        match stmt {
+            AstStatement::ExprStmt(expr) => self.translate_expr(expr).map(Statement::ExprStmt),
+            AstStatement::LetStmt(LetStatement { name, type_, value }) => {
+                let Some(type_) = type_ else { return Err(TranslationError::TypeInference)};
+                let type_ = self.parent.type_table.get(type_)?;
+                let value = value
+                    .map(|v| *v)
+                    .and_then(|expr| self.translate_expr(expr).ok())
+                    .map(Box::new);
+                Ok(Statement::LetStmt { name, type_, value })
+            }
+            AstStatement::Assignment { .. } => todo!(),
+            AstStatement::Return(expr) => self.translate_expr(expr).map(Statement::Return),
+            AstStatement::Break => todo!(),
+        }
+    }
+
+    fn translate_expr(&mut self, expr: AstExpression) -> Result<Expression, TranslationError> {
+        Ok(match expr {
+            AstExpression::Block(block) => Expression::Block(self.translate_block(block)?),
+            AstExpression::If { .. } => todo!(),
+            AstExpression::While { .. } => todo!(),
+            AstExpression::For { .. } => todo!(),
+            AstExpression::Unary { .. } => todo!(),
+            AstExpression::Binary { .. } => todo!(),
+            AstExpression::FnCall {
+                path,
+                params: ast_params,
+            } => {
+                let mut params = Vec::with_capacity(ast_params.capacity());
+                for param in ast_params {
+                    params.push(self.translate_expr(param)?);
+                }
+
+                let path = {
+                    let Some(path) = path.to_absolute(&self.module) else {
+                        todo!();
+                    };
+                    path
+                };
+
+                let Some(id) = self.parent.function_mapping.get(&path).copied() else {
+                    return Err(TranslationError::FunctionNotFound(path.clone()));
+                };
+                Expression::FnCall(id, params)
+            }
+            AstExpression::Var(_) => todo!(),
+            AstExpression::Literal(lit) => Expression::Literal(lit),
+        })
+    }
+}
