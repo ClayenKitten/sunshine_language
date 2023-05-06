99c95e857267676093bf5f2e0c096edbc7ee99a6
LowLevelVirtualMan <dima070704@yandex.ru>
2023-05-06 21:41:24 +0300

    FIx: VarId is unique to the whole function, not only the current block

--- src/hir/scope.rs
+++ src/hir/scope.rs
@@ -1,48 +1,64 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    cell::{Cell, RefCell},
+    collections::HashMap,
+    rc::Rc,
+};
 
 use crate::{hir::types::TypeId, Identifier};
 
 /// The scope is a portion of code that defines where variables are valid.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Scope(Rc<RefCell<ScopeInner>>);
+pub struct Scope {
+    inner: Rc<RefCell<ScopeInner>>,
+    latest_id: Rc<Cell<u32>>,
+}
 
 impl Scope {
     /// Creates a new top-level scope.
     pub fn new() -> Self {
-        Scope(Rc::new(RefCell::new(ScopeInner {
-            parent: None,
-            mapping: HashMap::new(),
-            types: HashMap::new(),
-        })))
+        Scope {
+            inner: Rc::new(RefCell::new(ScopeInner {
+                parent: None,
+                mapping: HashMap::new(),
+                types: HashMap::new(),
+            })),
+            latest_id: Rc::new(Cell::new(0)),
+        }
     }
 
     /// Creates a child scope.
     pub fn child(&self) -> Self {
-        Scope(Rc::new(RefCell::new(ScopeInner {
-            parent: Some(self.clone()),
-            mapping: HashMap::new(),
-            types: HashMap::new(),
-        })))
+        Scope {
+            inner: Rc::new(RefCell::new(ScopeInner {
+                parent: Some(self.clone()),
+                mapping: HashMap::new(),
+                types: HashMap::new(),
+            })),
+            latest_id: Rc::clone(&self.latest_id),
+        }
     }
 
     /// Inserts variable in the scope.
     pub fn insert(&mut self, var: Identifier, type_id: TypeId) -> VarId {
-        let mut scope = self.0.borrow_mut();
-        
-        let var_id = VarId(scope.mapping.len() as u32);
+        let mut scope = self.inner.borrow_mut();
+
+        let var_id = VarId(self.latest_id.get());
         scope.mapping.insert(var, var_id);
         scope.types.insert(var_id, type_id);
+        self.latest_id.set(var_id.0 + 1);
         var_id
     }
 
     /// Looks variable up in the scope or one of its parents.
     pub fn lookup(&self, var: &Identifier) -> Option<(VarId, TypeId)> {
-        let scope = self.0.borrow();
+        let scope = self.inner.borrow();
 
         let var_id = scope.mapping.get(var).copied();
         match var_id {
             Some(var_id) => {
-                let type_id = scope.types.get(&var_id)
+                let type_id = scope
+                    .types
+                    .get(&var_id)
                     .expect("Type should be defined for any `var_id` defined at the same scope");
                 Some((var_id, *type_id))
             }
@@ -55,7 +71,7 @@ impl Scope {
 
     /// Gets the parent scope if there is one.
     pub fn parent(&self) -> Option<Scope> {
-        self.0.borrow().parent.clone()
+        self.inner.borrow().parent.clone()
     }
 }
 
@@ -73,7 +89,7 @@ struct ScopeInner {
 }
 
 /// An id of local variable.
-/// 
+///
 /// These ids are only unique in the same function they were declared at.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct VarId(u32);
